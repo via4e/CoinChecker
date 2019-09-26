3f826fb0d6e376c3829a9daba464c6f31d1f726a
via4e <version-1@yandex.ru>
2019-09-26 20:45:58 +0300

    ui

--- static/js/ui.js
+++ static/js/ui.js
@@ -4,7 +4,7 @@ checkJquery();
 
 function checkJquery() {
     if (window.$){
-        console.log('ui.js');
+        //console.log('ui.js');
         startMe();
     } else {
         setTimeout(checkJquery, 50);
@@ -16,20 +16,6 @@ function startMe () {
         .done(function (data) {
             //console.log(`tickers:`, data)
             let html = '<table><thead><tr></tr></thead><tbody>'
-
-            //    exmo
-
-            for (let i in data) {
-                let exchange = data[i];
-                 if (i=='exmo'){
-                    html+='<tr><td>&nbsp;</td><tr><tr><td><strong>'+ i + '</strong></td><tr>' 
-                    for (let y in exchange) {
-                            html+='<tr><td>'+ exchange[y].name +'</td><td>'+ exchange[y].last + '</td><tr>'
-                    }
-                 }
-            }
-
-            // other
 
             for (let i in data) {
                 let exchange = data[i];
